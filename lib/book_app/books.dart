db386d86c741b8a87570ad3add793a9ce079031c
Sanjib <sanjib12sinha@gmail.com>
2021-06-25 16:44:58 +0530

    modifying book app

--- lib/book_app/books.dart
+++ lib/book_app/books.dart
@@ -2,11 +2,13 @@ import 'package:flutter/widgets.dart';
 
 class Books {
   final String id;
+  final List<String> categories;
   final String title;
   final String detail;
 
   Books({
     required this.id,
+    required this.categories,
     required this.title,
     required this.detail,
   });
